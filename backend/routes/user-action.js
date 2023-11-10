1cb57e50c7d42b1d6572c326af5c39ec43229321
Géczi Kornél <hungarokornel@gmail.com>
2023-11-10 19:47:12 +0100

    Implement user actions backend (editing and deleting users)

---
+++ backend/routes/user-action.js
@@ -0,0 +1,53 @@
+import express from 'express'
+import prisma from '../db.js'
+
+export const userActionRouter = express.Router();
+
+userActionRouter.put('/edit/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    if (isNaN(id)) {
+        return res.status(404).send("Not found.");
+    }
+
+    try {
+        var user = await prisma.user.update({
+            where: {
+                id
+            },
+            data: {
+                username: req.body.username,
+                role: req.body.role,
+                grade: req.body.grade,
+                class: req.body.class, // Kérdéses hogy szükség van-e rá
+                groupId: req.body.groupId
+            }
+        })
+    } catch {
+        return res.status(400).send()
+    }
+
+    res.status(200).json(user)
+})
+
+
+// Delete a user
+userActionRouter.delete("/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        return res.status(404).send("Not found.");
+    }
+
+    try {
+        await prisma.user.delete({
+            where: {
+                id: id
+            },
+        });
+    }
+    catch {
+        return res.status(404).send("Not found.");
+    }
+
+    res.send();
+})
