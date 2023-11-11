1c79e2fedbb18423defc5438c116e4624ba2f6a7
Györfi Marcell <gyorfimarcell@gmail.com>
2023-11-11 12:57:33 +0100

    Add modal

---
+++ client/online-competition/src/components/Modal.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useRef } from "react";
+import "../styles/Modal.css"
+
+const Modal = ({
+    isOpen,
+    children
+}) => {
+    const [isModalOpen, setModalOpen] = useState(isOpen);
+    const modalRef = useRef(null);
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Escape") {
+            handleCloseModal();
+        }
+    };
+
+    useEffect(() => {
+        setModalOpen(isOpen);
+    }, [isOpen]);
+
+    useEffect(() => {
+        const modalElement = modalRef.current;
+
+        if (modalElement) {
+            if (isModalOpen) {
+                modalElement.showModal();
+            } else {
+                modalElement.close();
+            }
+        }
+    }, [isModalOpen]);
+
+    return (
+        <dialog ref={modalRef} onKeyDown={handleKeyDown} className="modal">
+            <button className="modal-close-btn" onClick={handleCloseModal}>
+                X
+            </button>
+            {children}
+        </dialog>
+    );
+};
+
+export default Modal;
