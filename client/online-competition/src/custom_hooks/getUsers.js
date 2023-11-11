9de1e54061db6e416749a8fdd5f0f9f25804f07b
Ordinary56 <tbalogh695@gmail.com>
2023-11-11 13:32:50 +0100

    querying results works for now

--- client/online-competition/src/custom_hooks/getUsers.js
+++ client/online-competition/src/custom_hooks/getUsers.js
@@ -5,14 +5,14 @@ import {useState, useEffect} from "react";
  * @param {number} id - the users ID
  * @returns {Object} the user with the ID and the error, if ID is -1, returns all the users
  */
-export function useGetUsers(id=-1) {
+export function useGetData(role,id=-1) {
     const [data, setData] = useState(null);
     const [error,setError] = useState(null);
     useEffect(() => 
     {
-        const has_id = id === -1 ? "/" : `/user/${id}`
+        const has_id = id === -1 ? "/" : `/${id}`
         const fetchUsers = async () => {
-            await fetch("/api/admin" + has_id)
+            await fetch(`"/api/${role}/${has_id}`)
             .then(res => res.json())
             .then(data => setData(data))
             .catch(error => setError(error))
