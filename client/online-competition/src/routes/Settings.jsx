de4f349b46eeaa7c71aa62d08c25242a0d7dfcc7
Géczi Kornél <hungarokornel@gmail.com>
2023-11-12 18:44:00 +0100

    Implement settings design

--- client/online-competition/src/routes/Settings.jsx
+++ client/online-competition/src/routes/Settings.jsx
@@ -1,32 +1,24 @@
 import { useState } from "react";
-const Settings = () => 
-{
+import '../styles/Settings.css'
+
+const Settings = () => {
     const [title, setTitle] = useState("");
     const [desc, setDesc] = useState("");
-    
-    const ChangeSettings = (e) => 
-    {
+
+    const ChangeSettings = (e) => {
         e.preventDefault();
         //TODO: fetch and post the settings and get it from the database
     }
     return (
-          <>
+        <div className="settings">
+            <h2>Beállítások szerkesztése</h2>
             <form onSubmit={ChangeSettings}>
-                <fieldset>
-                    <label>Weboldal nevének megváltoztatása</label>
-                   <input type="text" placeholder="A weboldal új Neve" 
-                    onChange={(e) => setTitle( (prev) => prev = e.target.value)}/>
-                    <br/>
-                    <label>Weboldal leírásának  megváltoztatása</label>
-                   <input type="text" placeholder="A weboldal új leírása" 
-                    onChange={(e) => setDesc( (prev) => prev =  e.target.value)}/>
-                    <label>A weboldal új logója</label> 
-                   <br/>
-                  <input type="file" accept=".png, .jpg, .jpeg, .ico" />
-                  <input type="submit" value="Változtatás"/>
-                </fieldset>
+                <input type="text" placeholder="A weboldal új neve" onChange={(e) => setTitle((prev) => prev = e.target.value)} />
+                <input type="text" placeholder="A weboldal új leírása" onChange={(e) => setDesc((prev) => prev = e.target.value)} />
+                <input type="file" accept=".png, .jpg, .jpeg, .ico" />
+                <button>Bevitel</button>
             </form>
-        </>
+        </div>
     )
 }
 
