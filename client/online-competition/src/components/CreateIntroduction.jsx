202eecefb7c3b42d74138de691772d820643efb3
Géczi Kornél <hungarokornel@gmail.com>
2023-11-12 23:12:38 +0100

    Fix: Createintroduction

--- client/online-competition/src/components/CreateIntroduction.jsx
+++ client/online-competition/src/components/CreateIntroduction.jsx
@@ -5,40 +5,24 @@ import '../styles/editIntroduction.css'
 
 const IntroPage = () => {
   const [introductionText, setIntroductionText] = useState("");
-  const [uploadedImages, setUploadedImages] = useState([]);
-  const [uploadedDocuments, setUploadedDocuments] = useState([]);
   const [isIntroductionSubmitted, setIsIntroductionSubmitted] = useState(false);
 
   const handleTextChange = (event) => {
     setIntroductionText(event.target.value);
   };
 
-  const handleImageUpload = (event) => {
-    const files = event.target.files;
-    setUploadedImages([...uploadedImages, ...files]);
-  };
-
-  const handleDocumentUpload = (event) => {
-    const files = event.target.files;
-    setUploadedDocuments([...uploadedDocuments, ...files]);
-  };
-
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
     setIsIntroductionSubmitted(true);
     let formData = new FormData();
-    console.log(...uploadedImages);
-
-      uploadedImages.forEach(image => 
-          {
-              formData.append("files", image);
-          });
-        formData.append("text", introductionText);
-    FetchData("/api/intro/update","PUT",localStorage.getItem("access_token"), 
-        {
-            text : introductionText
-            })
-          .then(data => console.log(data));
+    formData.append("text", introductionText);
+    FetchData("/api/intro/update", "PUT", localStorage.getItem("access_token"),
+      {
+        text: introductionText
+      })
+      .then(data => console.log(data));
+      location.reload()
   }
   return (
     <div className='edit-introduction'>
@@ -46,15 +30,7 @@ const IntroPage = () => {
       {!isIntroductionSubmitted && (
         <form method='POST' encType='multipart/form-data'>
           <textarea placeholder='Bemutatkozó szöveg' value={introductionText} onChange={handleTextChange} />
-          <div className='filesUpload'>
-            <label htmlFor='images'>Képek feltöltése:</label>
-            <input type="file" id='images' name="files" accept="image/*" multiple onChange={handleImageUpload} />
-          </div>
-          <div className='filesUpload'>
-            <label htmlFor='documents'>Dokumentumok feltöltése:</label>
-            <input type="file" id='documents' accept=".pdf,.doc,.docx" name='files' multiple onChange={handleDocumentUpload} />
-          </div>
-          <button type='submit' onClick={handleSubmit}>Küldés</button>
+          <button type='submit' onClick={(e) => handleSubmit(e)}>Küldés</button>
         </form>
       )}
     </div>
